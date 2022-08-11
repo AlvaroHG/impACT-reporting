7120c6e638d4264e76066939912f21f52510fd0c
mikeg <mikeg@allenai.org>
2022-08-11 15:30:50 -0400

    Update about component

--- ui/src/pages/About.tsx
+++ ui/src/pages/About.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
 
+const sections = {"purpose": `The purpose of the AI2 ImpACT License is to support the use of AI for
+        the common good through Accountability, Collaboration, and Transparency (“ACT”).
+        The AI2 ImpACT License achieves these values by accepting certain ethical constraints
+        on the use of AI and the self-reporting of users regarding their use cases, current applications,
+        energy consumption, data set inputs, and funding sources, as captured and made publicly available
+        in user ImpACT Reports.`}
+
 export const About = () => {
     return (
         <React.Fragment>
-            <h1>About this Demo</h1>
-            <p>
-                This is a fresh application derived from the{' '}
-                <a href="https://github.com/allenai/skiff-template">Skiff Template</a>. Skiff
-                provides a <a href="https://www.python.org/">Python</a> based API and a UI
-                constructed with <a href="https://www.typescriptlang.org/">TypeScript</a>,{' '}
-                <a href="https://reactjs.org/">ReactJS</a>, and{' '}
-                <a href="https://github.com/allenai/varnish">Varnish</a>.
-            </p>
-            <p>
-                It's deployed to a Google managed Kubernetes cluster and provides DNS, log
-                aggregation, TLS and other capabilities out of the box, thanks to the{' '}
-                <a href="https://github.com/allenai/skiff">Skiff</a> project.
-            </p>
-            <p>
-                If you have any questions, concerns or feedback please don't hesitate to reach out.
-                You can open a{' '}
-                <a href="https://github.com/allenai/skiff-template/issues/new">Github Issue</a> or
-                contact us at <a href="mailto:reviz@allenai.org">reviz@allenai.org</a>.
-            </p>
-            <p>Smooth sailing!</p>
+            <h1>About</h1>
+            <p>{sections.purpose}</p>
+            <p>TODO: blog post contents, or brief summary of motivation, or just remove this page?</p>
         </React.Fragment>
     );
 };
