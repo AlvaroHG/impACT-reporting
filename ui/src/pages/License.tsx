ed5fa4e17a4a082b2bd2e1f578f8a6a6de73be6a
mikeg <mikeg@allenai.org>
2022-08-11 15:31:22 -0400

    Add license component

---
+++ ui/src/pages/License.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const License = () => {
+    return (
+        <React.Fragment>
+            <h1>License</h1>
+            <p>TODO: License body.
+            </p>
+        </React.Fragment>
+    );
+};
